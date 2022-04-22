dacbd49827850f304761c0dd55f6f8d9f1eecf1b
hudsonviana <hudson1206@gmail.com>
2022-04-22 00:25:42 -0300

    Inserindo CSS no React (CSS modules) - #06

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Frase from './components/Frase';
 import HelloWord from './components/HelloWord';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
@@ -8,6 +9,9 @@ function App() {
   const nome = 'Maria';
   return (
     <div className="App">
+      <h1>Testado CSS</h1>
+      <Frase />
+      <Frase />
       {/* <HelloWord /> */}
       <SayMyName nome="Eliane" />
       <SayMyName nome="Leppy" />
