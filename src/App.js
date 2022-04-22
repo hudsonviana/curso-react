b11808658cfecf9ffc1497b8e5d97f1c737e87ed
hudsonviana <hudson1206@gmail.com>
2022-04-21 22:39:00 -0300

    Entendendo o JSX - #03

--- src/App.js
+++ src/App.js
@@ -2,22 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const nome = 'Hudson Andrade Viana';
+  const url = 'https://via.placeholder.com/150';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Meu Primeiro App em React</h1>
+      <p>Olá, {nome.toUpperCase()}!</p>
+      <img src={url} />
     </div>
   );
 }
