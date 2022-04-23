581e75ac75ab26f6494a5a96d6ba1c011efcf5e7
hudsonviana <hudson1206@gmail.com>
2022-04-23 17:26:13 -0300

    React: Renderização de listas - #13

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import Condicional from './components/Condicional';
+import OutraLista from './components/OutraLista';
 
 function App() {
+
+  const meusItens = ['React', 'Vue', 'Angular'];
+  const meusItens2 = ['Fusca', 'Gol', 'Kombi'];
+
   return (
     <div className="App">
-      <h1>Teste de Evento Condicional</h1>
-    <Condicional />
+      <h1>Renderização de Listas</h1>
+      <OutraLista itens={meusItens} />
+      <OutraLista itens={[]} />
+      <OutraLista itens={meusItens2} />
     </div>
   );
 }
